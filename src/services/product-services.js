8603b9846452c05ba26803fe9a904e0e1b2b4e3e
Daniel Moya da Silva <danielm.silva@univem.edu.br>
2021-02-24 17:59:00 -0300

    Update product-services.js

--- src/services/product-services.js
+++ src/services/product-services.js
@@ -11,22 +11,38 @@ const ProductServices = {
         unity: data.unity,
         nameList: data.nameList,
         id_list: data.id_list,
-        status: false,
+        status: data.status,
         typeUnity: data.status
       })
         .then(response => {
           resolve(response)
-        }).catch(err => {
-          if (err.response) {
+        }).catch(error => {
+          if (error.response) {
             reject({
-              menssage: err.response.data.menssage
+              menssage: error.response.data.menssage
             })
           } else {
-            reject(err.response.data.menssage)
+            reject(error.response.data.menssage)
           }
         })
     })
+  },
+  getProductForList:(id_list)=>{
+    return new Promise((resolve, reject) => {
+      var product = [];
+      axios.get(`${server}/product`).then(response => {
+        for (let index = 0; index < response.data.length; index++) {
+          const element = response.data[index];
+          if (element.id_list == id_list) {
+            //console.log(element);
+            product.push(element)
+          }
+        }
+        resolve(product)
+      }).catch(error => {
+        reject(error.response)
+      })
+    })
   }
-
 }
 export default ProductServices;

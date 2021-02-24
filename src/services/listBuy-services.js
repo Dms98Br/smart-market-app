eacc692e0ab85deead5b27d2696945f7e3143539
Daniel Moya da Silva <danielm.silva@univem.edu.br>
2021-02-24 17:59:05 -0300

    Update listBuy-services.js

--- src/services/listBuy-services.js
+++ src/services/listBuy-services.js
@@ -38,6 +38,25 @@ const ListBuyServices = {
         reject(error.response)
       })
     })
+  },
+  getIdList: (id_user, id_list) => {
+    return new Promise((resolve, reject) => {      
+      axios.get(`${server}/lists`).then(response => {
+        for (let index = 0; index < response.data.length; index++) {
+          const element = response.data[index];
+          if (element.id_user == id_user) {
+            if (element._id.toString() === id_list) {
+              resolve(response.data[index])
+              //console.log('getList', response.data[index]);
+            }
+            //console.log(element);
+            //console.log(element);
+          }
+        }
+      }).catch(error => {
+        reject(error.response)
+      })
+    })
   }
 }
 
